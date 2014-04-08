960441f62aea1c76960477a9cc17c6fc9fdeacf7
Brian Foo <brian@youaremyjoy.org>
2014-04-08 01:06:10 -0400

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObliqueDigital::Application.config.secret_token = '7fa3111053b6c3a715900160ab0ef473daa090544dd603860f80713d24adc73870c6d08c124fc6c3e6b585ef89fc8a0f0ab729ab6a8d1e606a0568590cb23dfa'
